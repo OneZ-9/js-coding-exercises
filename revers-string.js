3cb0daaa0e534d1225d20786aabff318bf21acb5
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-11-11 22:44:11 +0530

    reverse string method 2

--- revers-string.js
+++ revers-string.js
@@ -1,21 +1,28 @@
 const btnEl = document.querySelector(".btn");
 const h1El = document.querySelector(".output");
-// console.log(btnEl);
 
 // with array operations
+/*
 function reverseString(str) {
-  //   const splittedStr = str.split("");
-  //   const reversedStr = splittedStr.reverse().join("");
-  //   console.log(`Reversed ${str} is ${reversedStr}`);
-
   const reversedStr = str.slice().split("").reverse().join("");
+
+  return reversedStr;
+}*/
+
+// without array methods, only for loop
+function reverseString(str) {
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
   return reversedStr;
 }
 
 const handleClick = () => {
   const str = prompt("Enter string to reverse");
 
-  h1El.textContent = reverseString(str);
+  h1El.textContent = `Reversed '${str}' to '${reverseString(str)}'`;
 };
 
 btnEl.addEventListener("click", handleClick);
